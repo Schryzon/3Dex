ad12846131941060619d22606e12dfa1f38f6c6f
Schryzon <widiyasajayananda@gmail.com>
2026-03-29 17:24:43 +0800

    fix: CORS allowed origins

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -24,7 +24,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: "Content-Security-Policy",
-            value: "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.midtrans.com https://*.gtflabs.io https://pay.google.com https://gwk.gopayapi.com https://accounts.google.com https://api.3dex.studio; img-src 'self' blob: data: https://*.midtrans.com https://api.3dex.studio https://storage.3dex.studio https://lh3.googleusercontent.com https://images.unsplash.com; connect-src 'self' https://api.3dex.studio https://storage.3dex.studio;",
+            value: "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.midtrans.com https://*.gtflabs.io https://pay.google.com https://gwk.gopayapi.com https://accounts.google.com https://api.3dex.studio; img-src 'self' blob: data: https://*.midtrans.com https://api.3dex.studio https://storage.3dex.studio https://lh3.googleusercontent.com https://images.unsplash.com https://picsum.photos https://fastly.picsum.photos; connect-src 'self' https://api.3dex.studio https://storage.3dex.studio;",
           },
         ],
       },
