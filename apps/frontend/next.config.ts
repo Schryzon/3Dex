2b9f68ace83e7cbb6dfe6857d67f0596041c7426
Schryzon <widiyasajayananda@gmail.com>
2026-03-29 18:40:13 +0800

    fix: fix CSP that prevents 3D rendering

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -24,7 +24,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: "Content-Security-Policy",
-            value: "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.midtrans.com https://*.gtflabs.io https://pay.google.com https://gwk.gopayapi.com https://accounts.google.com https://api.3dex.studio; img-src 'self' blob: data: https://*.midtrans.com https://api.3dex.studio https://storage.3dex.studio https://lh3.googleusercontent.com https://images.unsplash.com https://picsum.photos https://fastly.picsum.photos https://raw.githack.com; connect-src 'self' https://api.3dex.studio https://storage.3dex.studio https://raw.githack.com https://www.gstatic.com;",
+            value: "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.midtrans.com https://*.gtflabs.io https://pay.google.com https://gwk.gopayapi.com https://accounts.google.com https://api.3dex.studio; img-src 'self' blob: data: https://*.midtrans.com https://api.3dex.studio https://storage.3dex.studio https://lh3.googleusercontent.com https://images.unsplash.com https://picsum.photos https://fastly.picsum.photos https://raw.githack.com https://raw.githubusercontent.com; connect-src 'self' https://api.3dex.studio https://storage.3dex.studio https://raw.githack.com https://raw.githubusercontent.com https://www.gstatic.com;",
           },
         ],
       },
