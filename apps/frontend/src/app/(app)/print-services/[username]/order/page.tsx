24afc5beb34c598bcecd46b3b59a7303239ba0fb
Schryzon <widiyasajayananda@gmail.com>
2026-04-06 19:28:49 +0800

    fix: add use client to page.tsx that I coincidentally forgot to place in

--- apps/frontend/src/app/(app)/print-services/[username]/order/page.tsx
+++ apps/frontend/src/app/(app)/print-services/[username]/order/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useState, useEffect } from 'react';
 import { useRouter, useParams } from 'next/navigation';
 import { printService } from '@/lib/api/services/print.service';
